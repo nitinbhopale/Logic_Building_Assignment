64046b9a0d8638ecb50a61fdc8b5e1108db8440f
nitinbhopale <nitinbhopale53@gmail.com>
2023-10-07 22:54:28 +0530

    cmt

---
+++ Assignment-1/Question_3.c
@@ -0,0 +1,18 @@
+// Program to print 5 to 1 numbers on screen.
+
+#include<stdio.h>
+
+void Display()
+{
+    int i=0;
+    i = 5;
+    for(i = 5 ; i>0;i--)
+    {
+        printf("%d",i);
+    }
+}
+int main()
+{
+    Display();
+    return 0;
+}
